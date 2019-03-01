9580c2ec7964c5c8740ec2adb3b60f785daec35a
Brandon <brandoncyu@gmail.com>
2019-03-01 12:36:38 -0800

    initial repos added

---
+++ section-2-getting-started-with-java/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld
+{
+  //method
+  //"void" is the return type - it means we're not returning anything
+  //[] is array of strings
+  //args refers to the strings.
+  //public static are modifiers. You could also write "static public" - it does not matter the order.
+  //return type must be to the right.
+  //method name MUST be all lower case.
+  public static void main(String[] args)
+  {
+    //statement
+    System.out.println("Hello World");
+  }
+}
