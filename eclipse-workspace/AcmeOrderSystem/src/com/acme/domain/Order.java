2378f0cb7f2c1af113fdb5a9dbd2b699e54e05d0
Brandon <brandoncyu@gmail.com>
2019-03-11 17:13:10 -0700

    finished section 5

--- eclipse-workspace/AcmeOrderSystem/src/com/acme/domain/Order.java
+++ eclipse-workspace/AcmeOrderSystem/src/com/acme/domain/Order.java
@@ -1,10 +1,13 @@
+package com.acme.domain;
+import com.acme.utils.MyDate;
+
 public class Order {
 	MyDate orderDate;
 	double orderAmount = 0.00;
 	String customer;
 	String product;
 	int quantity;
-	static double taxRate;
+	public static double taxRate;
 	
 	public Order(MyDate d, double amt, String c) {
 		orderDate = d;
